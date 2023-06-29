fc19b83089fb0caac417e783e02c04679a9afc7a
kielkow <matheuskiel@fiorifer.com.br>
2023-06-29 12:44:46 -0300

    feat: check env vars

---
+++ src/env/index.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+    PORT: z.coerce.number().default(3333),
+    NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
+    HOST: z.string().default('0.0.0.0')
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+    throw new Error(`INVALID ENV VARS!\n ${JSON.stringify(_env.error.format())}`)
+}
+
+export const env = _env.data
