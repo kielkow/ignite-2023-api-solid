7c49986b481c0afb2317b1c89361453cac73acc5
kielkow <matheuskiel@fiorifer.com.br>
2023-07-13 11:28:24 -0300

    test: should hash user password upon registration

--- src/use-case/register.spec.ts
+++ src/use-case/register.spec.ts
@@ -1,5 +1,37 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+import { RegisterUseCase } from './register'
+
+describe('REGISTER USE CASE', () => {
+  const registerUseCase = new RegisterUseCase({
+    async create(data) {
+      return {
+        id: 'user-id',
+        name: data.name,
+        email: data.email,
+        password_hash: data.password_hash,
+        created_at: new Date(),
+      }
+    },
+
+    async findByEmail(email) {
+      return null
+    },
+  })
+
+  it('should hash user password upon registration', async () => {
+    const { user } = await registerUseCase.execute({
+      name: 'Jonh Doe',
+      email: 'jonhdoe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBeTruthy()
+  })
 })
