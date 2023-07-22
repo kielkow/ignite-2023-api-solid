c663b30d3af15cb4f5cb18b22137ca0290175745
kielkow <matheuskiel@fiorifer.com.br>
2023-07-22 12:31:09 -0300

    feat: in-memory-repository - findByUserIdOnDate

--- src/repositories/in-memory/in-memory-checkins-repository.ts
+++ src/repositories/in-memory/in-memory-checkins-repository.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'crypto'
 import { CheckIn, Prisma } from '@prisma/client'
 
 import { CheckInsRepository } from '../check-ins-repository'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
 	public checkIns: CheckIn[] = []
@@ -20,5 +21,26 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 		this.checkIns.push(checkIn)
 
 		return checkIn
+	}
+
+	async findByUserIdOnDate(
+		userId: string,
+		date: Date,
+	): Promise<CheckIn | null> {
+		const startOfTheDay = dayjs(date).startOf('date')
+		const endOfTheDay = dayjs(date).endOf('date')
+
+		const checkInOnSameDate = this.checkIns.find((checkIn) => {
+			const checkInDate = dayjs(checkIn.created_at)
+
+			const isOnSameDate =
+				checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+			return checkIn.user_id === userId && isOnSameDate
+		})
+
+		if (!checkInOnSameDate) return null
+
+		return checkInOnSameDate
 	}
 }
