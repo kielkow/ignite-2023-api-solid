553840458d4dd2cd4688fbf8c91ef6ca961617bc
kielkow <matheuskiel@fiorifer.com.br>
2023-07-27 14:40:38 -0300

    feat: fetch user check ins history

---
+++ src/use-case/usecases/fetch-member-check-ins-history/fetch-user-check-ins-history.ts
@@ -0,0 +1,23 @@
+import { CheckIn } from '@prisma/client'
+
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+	userId: string
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+	checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+	constructor(private checkInsRepository: CheckInsRepository) {}
+
+	async execute({
+		userId,
+	}: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+		const checkIns = await this.checkInsRepository.findByUserId(userId)
+
+		return { checkIns }
+	}
+}
