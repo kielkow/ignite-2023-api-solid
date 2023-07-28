58feb1086a51dea67f72e0fe634f316a3655ae6c
kielkow <matheuskiel@fiorifer.com.br>
2023-07-28 09:51:44 -0300

    test: should be able to paginated fetch checkins history

--- src/use-case/usecases/fetch-member-check-ins-history/fetch-user-check-ins-history.spec.ts
+++ src/use-case/usecases/fetch-member-check-ins-history/fetch-user-check-ins-history.spec.ts
@@ -26,12 +26,33 @@ describe('FETCH USER CHECK-INS HISTORY USE CASE', () => {
 
 		const { checkIns } = await sut.execute({
 			userId: 'user-id',
+			page: 1,
 		})
 
 		expect(checkIns).toHaveLength(2)
 		expect(checkIns).toEqual([
 			expect.objectContaining({ gym_id: 'gym-01' }),
 			expect.objectContaining({ gym_id: 'gym-02' }),
+		])
+	})
+
+	it('should be able to fetch paginated check-in history', async () => {
+		for (let i = 1; i <= 22; i++) {
+			await checkInsRepository.create({
+				user_id: 'user-id',
+				gym_id: `gym-${i}`,
+			})
+		}
+
+		const { checkIns } = await sut.execute({
+			userId: 'user-id',
+			page: 2,
+		})
+
+		expect(checkIns).toHaveLength(2)
+		expect(checkIns).toEqual([
+			expect.objectContaining({ gym_id: 'gym-21' }),
+			expect.objectContaining({ gym_id: 'gym-22' }),
 		])
 	})
 })
