3dd98db6bc245e7c1c4cd5dcb6d5df30db6d3121
kielkow <matheuskiel@fiorifer.com.br>
2023-08-02 11:24:37 -0300

    test: should not be able to validate invalid check-in

--- src/use-case/usecases/validate-check-in/validate-check-in.spec.ts
+++ src/use-case/usecases/validate-check-in/validate-check-in.spec.ts
@@ -4,6 +4,8 @@ import { ValidateCheckInUseCase } from './validate-check-in'
 
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
 
+import { ResourceNotFoundError } from '@/use-case/errors/resource-not-found-error'
+
 describe('VALIDATE CHECK-IN USE CASE', () => {
 	let checkInsRepository: InMemoryCheckInsRepository,
 		sut: ValidateCheckInUseCase
@@ -25,5 +27,11 @@ describe('VALIDATE CHECK-IN USE CASE', () => {
 		expect(checkInsRepository.checkIns[0].validated_at).toEqual(
 			expect.any(Date),
 		)
+	})
+
+	it('should not be able to validate invalid check-in', async () => {
+		await expect(() =>
+			sut.execute({ checkInId: 'invalid-check-in' }),
+		).rejects.toBeInstanceOf(ResourceNotFoundError)
 	})
 })
