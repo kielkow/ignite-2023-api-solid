6261b7cdde39bc3f480161a275f4afb10ff3ffd1
kielkow <matheuskiel@fiorifer.com.br>
2023-08-04 10:57:55 -0300

    feat: prisma checkins repository

---
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -0,0 +1,70 @@
+import dayjs from 'dayjs'
+import { CheckIn, Prisma } from '@prisma/client'
+
+import { CheckInsRepository } from '../check-ins-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaCheckInsRepository implements CheckInsRepository {
+	public checkIns: CheckIn[] = []
+
+	async create(data: Prisma.CheckInUncheckedCreateInput) {
+		const checkIn = await prisma.checkIn.create({ data })
+
+		return checkIn
+	}
+
+	async findById(id: string): Promise<CheckIn | null> {
+		const checkIn = await prisma.checkIn.findUnique({
+			where: { id },
+		})
+
+		return checkIn
+	}
+
+	async findByUserIdOnDate(
+		userId: string,
+		date: Date,
+	): Promise<CheckIn | null> {
+		const startOfTheDay = dayjs(date).startOf('date')
+		const endOfTheDay = dayjs(date).endOf('date')
+
+		const checkInOnSameDate = await prisma.checkIn.findFirst({
+			where: {
+				user_id: userId,
+				created_at: {
+					gte: startOfTheDay.toDate(),
+					lte: endOfTheDay.toDate(),
+				},
+			},
+		})
+
+		return checkInOnSameDate
+	}
+
+	async findByUserId(userId: string, page: number): Promise<CheckIn[]> {
+		const checkIns = await prisma.checkIn.findMany({
+			where: { user_id: userId },
+			take: 20,
+			skip: (page - 1) * 20,
+		})
+
+		return checkIns
+	}
+
+	async countByUserId(userId: string): Promise<number> {
+		const checkIns = await prisma.checkIn.count({
+			where: { user_id: userId },
+		})
+
+		return checkIns
+	}
+
+	async save(data: CheckIn): Promise<CheckIn> {
+		const checkIn = await prisma.checkIn.update({
+			where: { id: data.id },
+			data,
+		})
+
+		return checkIn
+	}
+}
