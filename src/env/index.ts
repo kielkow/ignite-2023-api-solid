649dc52c3e674a053b8b49f25135a2a7f872ee37
kielkow <matheuskiel@fiorifer.com.br>
2023-08-08 13:24:06 -0300

    feat: setup jwt secret

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
 	PORT: z.coerce.number().default(3333),
 	NODE_ENV: z.enum(['dev', 'test', 'prod']).default('dev'),
 	HOST: z.string().default('0.0.0.0'),
+	JWT_SECRET: z.string().default('apisolid'),
 })
 
 const _env = envSchema.safeParse(process.env)
