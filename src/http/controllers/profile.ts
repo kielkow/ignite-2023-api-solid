100f7c85682a3f6c85a70bf250b7d79c596e3f43
kielkow <matheuskiel@fiorifer.com.br>
2023-08-09 10:46:34 -0300

    feat: get user profile info

---
+++ src/http/controllers/profile.ts
@@ -0,0 +1,9 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+	await request.jwtVerify()
+
+	const { user } = request
+
+	return reply.status(200).send(user)
+}
