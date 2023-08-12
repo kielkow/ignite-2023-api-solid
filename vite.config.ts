c7131efde686e9dbfc1f22b0d0041be41284e220
kielkow <matheuskiel@fiorifer.com.br>
2023-08-12 14:32:28 -0300

    chore: config test environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { configDefaults, defineConfig } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
-  test: {
-    exclude: [...configDefaults.exclude, 'build'],
-  },
+	plugins: [tsconfigPaths()],
+	test: {
+		exclude: [...configDefaults.exclude, 'build'],
+		environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+	},
 })
