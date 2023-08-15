4dd3ad4d2917ce721dabbaceaea05ca846a18421
kielkow <matheuskiel@fiorifer.com.br>
2023-08-15 09:21:59 -0300

    refact

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,8 @@
 import { FastifyInstance } from 'fastify'
 
-import { register } from './controllers/register'
-import { authenticate } from './controllers/authenticate'
-import { profile } from './controllers/profile'
+import { register } from './controllers/register/register'
+import { authenticate } from './controllers/authenticate/authenticate'
+import { profile } from './controllers/profile/profile'
 
 import { verifyJWT } from './middlewares/verify-jwt'
 
