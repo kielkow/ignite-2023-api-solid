89c1bf8f743a235a5079607ab033c30bcaddba46
kielkow <matheuskiel@fiorifer.com.br>
2023-08-19 13:10:19 -0300

    feat: routes search and nearby gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,9 +3,14 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create/create'
+import { search } from './search/search'
+import { nearby } from './nearby/nearby'
 
 export async function gymsRoutes(app: FastifyInstance) {
 	app.addHook('onRequest', verifyJWT)
 
 	app.post('/gyms', create)
+
+	app.get('/gyms/search', search)
+	app.get('/gyms/nearby', nearby)
 }
