b2503a23c256e52536be418cb90b62668ad128b0
kielkow <matheuskiel@fiorifer.com.br>
2023-08-19 13:09:59 -0300

    feat: nearby gyms controller

---
+++ src/http/controllers/gyms/nearby/nearby.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+import { makeFetchNearbyGymsUsecase } from '@/use-case/factories/gyms/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+	const nearbyGymsQuerySchema = z.object({
+		latitude: z.number().refine((value) => {
+			return Math.abs(value) <= 90
+		}),
+		longitude: z.number().refine((value) => {
+			return Math.abs(value) <= 180
+		}),
+	})
+
+	const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+	const fetchNearbyGymsUseCase = makeFetchNearbyGymsUsecase()
+
+	const { gyms } = await fetchNearbyGymsUseCase.execute({
+		userLatitude: latitude,
+		userLongitude: longitude,
+	})
+
+	return reply.status(200).send({ gyms })
+}
