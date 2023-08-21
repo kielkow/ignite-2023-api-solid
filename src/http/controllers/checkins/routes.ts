9a9e05cf14c335d9f9464e186d2b167dea7088e9
kielkow <matheuskiel@fiorifer.com.br>
2023-08-21 12:40:29 -0300

    feat: checkins routes

--- src/http/controllers/checkins/routes.ts
+++ src/http/controllers/checkins/routes.ts
@@ -3,9 +3,17 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 
 import { create } from './create/create'
+import { history } from './history/history'
+import { metrics } from './metrics/metrics'
+import { validate } from './validate/validate'
 
 export async function checkInsRoutes(app: FastifyInstance) {
 	app.addHook('onRequest', verifyJWT)
 
 	app.post('/checkins', create)
+
+	app.get('/checkins/history', history)
+	app.get('/checkins/metrics', metrics)
+
+	app.patch('/checkins/:checkInId/validate', validate)
 }
