e10d9f5f0d8a356cb827929eb161a1c7281c5f81
kielkow <matheuskiel@fiorifer.com.br>
2023-08-21 12:11:38 -0300

    feat: add reponse of erros to create checkin controller

--- src/http/controllers/checkins/create/create.ts
+++ src/http/controllers/checkins/create/create.ts
@@ -3,6 +3,10 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 
 import { makeCreateCheckInUsecase } from '@/use-case/factories/checkins/make-create-check-in-use-case'
 
+import { MaxDistanceError } from '@/use-case/errors/max-distance-error'
+import { ResourceNotFoundError } from '@/use-case/errors/resource-not-found-error'
+import { MaxNumberOfCheckInsError } from '@/use-case/errors/max-number-of-check-ins-error'
+
 export async function create(request: FastifyRequest, reply: FastifyReply) {
 	const createCheckInBodySchema = z.object({
 		gymId: z.string().uuid().nonempty(),
@@ -18,14 +22,30 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
 		request.body,
 	)
 
-	const createCheckInUseCase = makeCreateCheckInUsecase()
+	try {
+		const createCheckInUseCase = makeCreateCheckInUsecase()
 
-	await createCheckInUseCase.execute({
-		gymId,
-		userId: request.user.sub,
-		userLatitude: latitude,
-		userLongitude: longitude,
-	})
+		await createCheckInUseCase.execute({
+			gymId,
+			userId: request.user.sub,
+			userLatitude: latitude,
+			userLongitude: longitude,
+		})
+	} catch (error) {
+		if (error instanceof MaxDistanceError) {
+			return reply.status(400).send({ message: error.message })
+		}
+
+		if (error instanceof ResourceNotFoundError) {
+			return reply.status(409).send({ message: error.message })
+		}
+
+		if (error instanceof MaxNumberOfCheckInsError) {
+			return reply.status(400).send({ message: error.message })
+		}
+
+		throw error
+	}
 
 	return reply.status(201).send()
 }
