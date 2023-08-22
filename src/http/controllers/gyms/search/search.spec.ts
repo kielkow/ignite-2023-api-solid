15aefd3b844f5ee263f507b8887e7bbc95c302b8
kielkow <matheuskiel@fiorifer.com.br>
2023-08-22 09:49:41 -0300

    test: should be able to search gyms

--- src/http/controllers/gyms/search/search.spec.ts
+++ src/http/controllers/gyms/search/search.spec.ts
@@ -1,7 +1,10 @@
 import { app } from '@/app'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-describe('SEARCH GYMS CONTROLLER', () => {
+import { createAuthenticateUser } from '@/utils/test/create-authenticate-user'
+
+describe('SEARCH GYM CONTROLLER', () => {
 	beforeAll(async () => {
 		await app.ready()
 	})
@@ -10,5 +13,44 @@ describe('SEARCH GYMS CONTROLLER', () => {
 		await app.close()
 	})
 
-	it('should be able to search gyms', async () => {})
+	it('should be able to search gyms', async () => {
+		const { token } = await createAuthenticateUser(app)
+
+		await request(app.server)
+			.post('/gyms')
+			.send({
+				title: 'Temple Gym',
+				description: 'Muscle Gym Pro',
+				phone: '7070-7070',
+				latitude: -27.2092052,
+				longitude: -49.6401091,
+			})
+			.set('Authorization', `Bearer ${token}`)
+
+		await request(app.server)
+			.post('/gyms')
+			.send({
+				title: 'Contest Gym',
+				description: 'Muscle Gym Pro',
+				phone: '7171-7171',
+				latitude: -27.2092052,
+				longitude: -49.6401091,
+			})
+			.set('Authorization', `Bearer ${token}`)
+
+		const response = await request(app.server)
+			.get('/gyms/search')
+			.query({
+				q: 'Temple',
+			})
+			.set('Authorization', `Bearer ${token}`)
+
+		expect(response.statusCode).toEqual(200)
+		expect(response.body.gyms).toHaveLength(1)
+		expect(response.body.gyms).toEqual([
+			expect.objectContaining({
+				title: 'Temple Gym',
+			}),
+		])
+	})
 })
