949058bcf375f63f58993150f6db87282e3dec47
kielkow <matheuskiel@fiorifer.com.br>
2023-08-22 09:57:43 -0300

    fix: convert latitude and longitude to number

--- src/http/controllers/gyms/nearby/nearby.ts
+++ src/http/controllers/gyms/nearby/nearby.ts
@@ -5,10 +5,10 @@ import { makeFetchNearbyGymsUsecase } from '@/use-case/factories/gyms/make-fetch
 
 export async function nearby(request: FastifyRequest, reply: FastifyReply) {
 	const nearbyGymsQuerySchema = z.object({
-		latitude: z.number().refine((value) => {
+		latitude: z.coerce.number().refine((value) => {
 			return Math.abs(value) <= 90
 		}),
-		longitude: z.number().refine((value) => {
+		longitude: z.coerce.number().refine((value) => {
 			return Math.abs(value) <= 180
 		}),
 	})
