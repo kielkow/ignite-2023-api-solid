a7ebeee54f94599ba31466cab4a9b88872945be0
kielkow <matheuskiel@fiorifer.com.br>
2023-08-23 11:22:40 -0300

    test: should be able to create checkin

--- src/http/controllers/checkins/create/create.spec.ts
+++ src/http/controllers/checkins/create/create.spec.ts
@@ -1,5 +1,9 @@
 import { app } from '@/app'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import request from 'supertest'
+import { prisma } from '@/lib/prisma'
+import { afterAll, beforeAll, describe, it, expect } from 'vitest'
+
+import { createAuthenticateUser } from '@/utils/test/create-authenticate-user'
 
 describe('CREATE CHECKIN CONTROLLER', () => {
 	beforeAll(async () => {
@@ -10,5 +14,28 @@ describe('CREATE CHECKIN CONTROLLER', () => {
 		await app.close()
 	})
 
-	it('should be able to create checkin', async () => {})
+	it('should be able to create checkin', async () => {
+		const { token } = await createAuthenticateUser(app)
+
+		const { id: gymId } = await prisma.gym.create({
+			data: {
+				title: 'Gym',
+				description: 'Muscle Gym Pro',
+				phone: '7070-7070',
+				latitude: -27.2092052,
+				longitude: -49.6401091,
+			},
+		})
+
+		const response = await request(app.server)
+			.post('/checkins')
+			.send({
+				gymId,
+				latitude: -27.2092052,
+				longitude: -49.6401091,
+			})
+			.set('Authorization', `Bearer ${token}`)
+
+		expect(response.statusCode).toEqual(201)
+	})
 })
