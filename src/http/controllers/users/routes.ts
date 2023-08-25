600da85189d1089f437507d49d1be52420d6fe94
kielkow <matheuskiel@fiorifer.com.br>
2023-08-25 12:12:13 -0300

    feat: refresh token route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { register } from './register/register'
 import { authenticate } from './authenticate/authenticate'
 import { profile } from './profile/profile'
+import { refresh } from './refresh/refresh'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
 
@@ -10,6 +11,8 @@ export async function usersRoutes(app: FastifyInstance) {
 	// Not Authenticated
 	app.post('/users', register)
 	app.post('/sessions', authenticate)
+
+	app.patch('/token/refresh', refresh)
 
 	// Authenticated
 	app.get('/me', { onRequest: [verifyJWT] }, profile)
