a8990d0d40d3eaa92ff7648411453ecbdfb7cfb3
kielkow <matheuskiel@fiorifer.com.br>
2023-08-26 12:03:27 -0300

    feat: add user role to jwt token on creation

--- src/http/controllers/users/refresh/refresh.ts
+++ src/http/controllers/users/refresh/refresh.ts
@@ -3,12 +3,12 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 export async function refresh(request: FastifyRequest, reply: FastifyReply) {
 	await request.jwtVerify({ onlyCookie: true })
 
-	const { sub } = request.user
+	const { sub, role } = request.user
 
-	const token = await reply.jwtSign({}, { sign: { sub } })
+	const token = await reply.jwtSign({ role }, { sign: { sub } })
 
 	const refreshToken = await reply.jwtSign(
-		{},
+		{ role },
 		{ sign: { sub, expiresIn: '7d' } },
 	)
 
