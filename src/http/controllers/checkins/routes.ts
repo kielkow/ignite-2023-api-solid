1025e1da04c178626c6704b98a827e670f677f26
kielkow <matheuskiel@fiorifer.com.br>
2023-08-26 12:04:22 -0300

    feat: middleware to verify user role on routes

--- src/http/controllers/checkins/routes.ts
+++ src/http/controllers/checkins/routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 import { create } from './create/create'
 import { history } from './history/history'
@@ -15,5 +16,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
 	app.get('/checkins/history', history)
 	app.get('/checkins/metrics', metrics)
 
-	app.patch('/checkins/:checkInId/validate', validate)
+	app.patch(
+		'/checkins/:checkInId/validate',
+		{ onRequest: [verifyUserRole('ADMIN')] },
+		validate,
+	)
 }
